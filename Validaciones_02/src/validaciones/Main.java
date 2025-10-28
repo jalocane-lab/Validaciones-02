6aa92e587b79616303ab10528893b7864dda3c1e
Javier <javierlocane@gmail.com>
2025-10-28 17:16:25 -0300

    Commit para Github

--- Validaciones_02/src/validaciones/Main.java
+++ Validaciones_02/src/validaciones/Main.java
@@ -1,10 +1,26 @@
 package validaciones;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner (System.in);
+		ArrayList<String> nombres = new ArrayList<>();
+	
+		String nombre;
+		String correo;
+		String contraseña;
 
+		System.out.println("-------------------------------------"  );
+		System.out.println("-- Sistema de gestión de usuarios --"  );
+		System.out.println("-------------------------------------"  );
+		System.out.println("");
+		
+		
+		
+		sc.close();
 	}
 
 }
