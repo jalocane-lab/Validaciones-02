53e1e96ce7a0534946b99b8313b7dc6fc5e3a09d
Javier <javierlocane@gmail.com>
2025-11-04 14:59:01 -0300

    Se Crea la clase "Usuario"

--- Validaciones_02/src/validaciones/Main.java
+++ Validaciones_02/src/validaciones/Main.java
@@ -1,17 +1,13 @@
 package validaciones;
-
-import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
 		ArrayList<String> nombres = new ArrayList<>();
-	
-		String nombre;
-		String correo;
-		String contraseña;
 
 		System.out.println("-------------------------------------"  );
 		System.out.println("-- Sistema de gestión de usuarios --"  );
---
+++ Validaciones_02/src/validaciones/Usuario.java
@@ -0,0 +1,17 @@
+package validaciones;
+
+public class Usuario {
+	
+	String nombre;
+	String correo;
+	String contraseña;
+	
+	public Usuario(String nombre, String correo, String contraseña) {
+		this.nombre = nombre;
+		this.correo = correo;
+		this.contraseña = contraseña;
+	}
+	
+	
+
+}
