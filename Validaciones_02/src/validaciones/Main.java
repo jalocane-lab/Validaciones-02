5c866e1f7010f38adb73b90ef754083c2da5d868
Javier <javierlocane@gmail.com>
2025-11-04 16:02:42 -0300

    Commit con datos forzados

--- Validaciones_02/src/validaciones/Main.java
+++ Validaciones_02/src/validaciones/Main.java
@@ -1,19 +1,27 @@
 package validaciones;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.*;
 
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
-		ArrayList<String> nombres = new ArrayList<>();
+		ArrayList<Usuario> lista = new ArrayList<>();
 
 		System.out.println("-------------------------------------"  );
 		System.out.println("-- Sistema de gestión de usuarios --"  );
 		System.out.println("-------------------------------------"  );
 		System.out.println("");
+
 		
+		lista.add(new Usuario ("Ricardo","ricardo@yahoo.com", "123456"));
+		lista.add(new Usuario ("Maria","maria@yahoo.com", "abcdef"));
+		
+		for (Usuario usuarios : lista) {
+			usuarios.mostrarInfo();
+		}
 		
 		
 		sc.close();
--- Validaciones_02/src/validaciones/Usuario.java
+++ Validaciones_02/src/validaciones/Usuario.java
@@ -2,16 +2,42 @@ package validaciones;
 
 public class Usuario {
 	
-	String nombre;
-	String correo;
-	String contraseña;
+	private String nombre;
+	private String correo;
+	private String contraseña;
 	
 	public Usuario(String nombre, String correo, String contraseña) {
 		this.nombre = nombre;
 		this.correo = correo;
 		this.contraseña = contraseña;
 	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
 	
-	
+	public void mostrarInfo() {
+		System.out.println("Nombre de usuario: " + nombre + " Correo electronico: " + correo);
+	}
 
 }
